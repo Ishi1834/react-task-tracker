180ea504b8a78126ea4efe69b2a9305d6edad1ec
Ismail Mohamad <ikmohamad19@gmail.com>
2021-08-08 20:37:52 +0100

    Added task, tasks and did the button, installed react icons to be used. At minute 50

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id:1,
+        text: 'Clean room',
+        day: 'August 8th at 3.00pm',
+        reminder: true,
+    },
+    {
+        id:2,
+        text: 'Ride bike',
+        day: 'August 8th at 5.00pm',
+        reminder: true,
+    },
+    {
+        id:1,
+        text: 'Buy food',
+        day: 'August 8th at 3.00pm',
+        reminder: false,
+    },
+])
+
   return (
     <div className='container'>
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
