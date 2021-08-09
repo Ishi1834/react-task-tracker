ad0615142dfdf87dee6ce30d094847f2b8b73dba
Ismail Mohamad <ikmohamad19@gmail.com>
2021-08-09 02:32:33 +0100

    Created the Add Task component

--- src/App.js
+++ src/App.js
@@ -26,6 +26,11 @@ const App = () => {
 ])
 
 //Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() *10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
 
 //delete Task
 const deleteTask = (id) => {
@@ -40,7 +45,7 @@ const toggleReminder = (id) => {
   return (
     <div className='container'>
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length >0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ( 'No Tasks To Show')}
     </div>
   );
